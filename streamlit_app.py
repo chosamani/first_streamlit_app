d68ae0a34a3d0ae70c6f53217d08f66a9638d2a0
chosamani <53465496+chosamani@users.noreply.github.com>
2024-02-14 17:20:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,9 @@ streamlit.dataframe(fruits_to_show)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+streamlit.header('Fruityvice Fruit Advance!')
+
+import requests
+fruityvice_response=request.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
