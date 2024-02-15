5c1f229b890d381c779ac96840d12e584eb92c8f
chosamani <53465496+chosamani@users.noreply.github.com>
2024-02-15 14:44:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,13 +46,14 @@ import requests
 
 #fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 #New selection to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice')
 
 def get_fruityvice_data(this_fruit_choice):
  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "this_fruit_choice")
  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())  
  return fruityvice_normalized
  #create the repeatable code block called a function
-    streamlit.header('Fruityvice Fruit Advice')
+    
 try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 #streamlit.write('The user entered', fruit_choice)
