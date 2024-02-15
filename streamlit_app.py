ae73b0b54988e2a6c8671f9998e9020f5b43842c
chosamani <53465496+chosamani@users.noreply.github.com>
2024-02-15 15:45:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,8 +55,8 @@ def get_fruityvice_data(this_fruit_choice):
  #create the repeatable code block called a function
  streamlit.header('Fruityvice Fruit Advice')   
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-streamlit.write('The user entered', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
+#streamlit.write('The user entered', fruit_choice)
 if not fruit_choice:
   streamlit.error("Please select fruit to get information")
 else:
